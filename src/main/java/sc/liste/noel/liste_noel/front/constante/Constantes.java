7a1b0d9b18691bcf0bef6b2938ffc87c03a01690
Tenalic <stephane.cocquebert@gmail.com>
2025-10-13 21:59:12 +0200

    menage

--- src/main/java/sc/liste/noel/liste_noel/front/constante/Constantes.java
+++ src/main/java/sc/liste/noel/liste_noel/front/constante/Constantes.java
@@ -2,78 +2,21 @@ package sc.liste.noel.liste_noel.front.constante;
 
 public class Constantes {
 
-    public static String MATCH_NON_FINI = "Incomplete";
-
     public static int RETOUR_API_OK = 0;
 
     public static int RETOUR_API_KO = 1;
 
-    public static final int CODE_FRANCAIS = 1;
-
-    public static final int CODE_ANGLAIS = 2;
-
-    // messages erreurs
     public static final String CONNEXION_KEY = "connexion";
-    public static final String CONNEXION_FR = "Vous devez vous connecter";
-    public static final String CONNEXION_EN = "You must log in";
-    public static final String MESSAGE_DEFAUT_FR = "Message inconnu";
-    public static final String MESSAGE_DEFAUT_EN = "Unknown message";
     public static final String MDP_NOT_EQUALS_KEY = "mdpNotEquals";
-    public static final String MDP_NOT_EQUALS_FR = "Les mots de passe ne correspondent pas";
-    public static final String MDP_NOT_EQUALS_EN = "The passwords do not match";
     public static final String CGU_NON_ACCEPTE_KEY = "cguNonAccepte";
     public static final String EMAIL_NON_ACCEPTE_KEY = "emailMalForme";
-    public static final String EMAIL_NON_ACCEPTE_FR = "L'email n'est pas correct, il doit être du style : \"user@example.com\"";
-    public static final String CGU_NON_ACCEPTE_FR = "Vous devez accepter les conditions générale d’utilisation pour vous inscrire";
-    public static final String CGU_NON_ACCEPTE_EN = "You must accepte the term of use to register";
     public static final String NEW_MDP_NOT_EQUALS_KEY = "newMdpNotEquals";
-    public static final String NEW_MDP_NOT_EQUALS_FR = "Les nouveaux mots de passe ne correspondent pas";
-    public static final String NEW_MDP_NOT_EQUALS_EN = "New passwords do not match";
     public static final String CONNEXION_FAIL_KEY = "connexionFail";
-    public static final String CONNEXION_FAIL_FR = "Email ou mot de passe incorrect";
-    public static final String CONNEXION_FAIL_EN = "Incorrect email or password";
     public static final String ERREUR_GENERIQUE_KAY = "erreurGenerique";
-    public static final String ERREUR_GENERIQUE_FR = "Une erreur est survenue";
-    public static final String ERREUR_GENERIQUE_EN = "An error has occurred";
-    public static final String ERREUR_RESULTAT_KEY = "erreruResultat";
-    public static final String ERREUR_RESULTAT_FR = "Une erreur est survenue lors de l'envoi du résultat du match";
-    public static final String ERREUR_RESULTAT_EN = "An error occurred when sending the match result";
-    public static final String RESULTAT_EMPRTY_KEY = "resultatEmpty";
-    public static final String RESULTAT_EMPRTY_FR = "Veuillez sélectionner un résultat";
-    public static final String RESULTAT_EMPRTY_EN = "Please select a result";
-    public static final String ERREUR_RECUP_INFOS_TOURNOI_KEY = "erreurRecupResultat";
-    public static final String ERREUR_RECUP_INFOS_TOURNOI_FR = "Une erreur est survenu lors de la récupération des infos du tournois selectionné";
-    public static final String ERREUR_RECUP_INFOS_TOURNOI_EN = "An error occurred while retrieving information from the selected tournament";
-    public static final String DROP_LOCK_KEY = "dropLock";
-    public static final String DROP_LOCK_FR = "Le scorekeeper a bloqué la possibilité de drop, merci de vous rendre à la table d'arbitrage";
-    public static final String DROP_LOCK_EN = "The scorekeeper has blocked the possibility of drop, please go to the arbitration table";
-    public static final String ERROR_DROP_KEY = "errorDrop";
-    public static final String ERROR_DROP_FR = "Une erreur est survenu lors de la tentative de drop";
-    public static final String ERROR_DROP_EN = "An error occurred while trying to drop";
-    public static final String ERROR_TOURNOIS_INSCRIT_KEY = "errorTournoisInscrit";
-    public static final String ERROR_TOURNOIS_INSCRIT_FR = "Une erreur est survenu lors de la récupération des tournois inscrits";
-    public static final String ERROR_TOURNOIS_INSCRIT_EN = "An error occurred while retrieving registered tournaments";
-    public static final String ID_EMPTY_KEY = "idEmpty";
-    public static final String ID_EMPTY_FR = "L'id konami n'est pas rempli";
-    public static final String ID_EMPTY_EN = "The id konami is not filled";
-    public static final String COSSY_LETTRE_KEY = "cossyLettre";
-    public static final String COSSY_LETTRE_FR = "Le cossy ne contien pas que des chiffres";
-    public static final String COSSY_LETTRE_EN = "The cossy does not contain only numbers";
-    public static final String COSSY_TAILLE_KEY = "cossyTaille";
-    public static final String COSSY_TAILLE_FR = "L'id konami ne fait pas la bonne taille, il doit faire une taille de 10 chiffres";
-    public static final String COSSY_TAILLE_EN = "The id konami is not the right size, it must be a size of 10 digits";
+
     public static final String COMPTE_EXISTE_KEY = "compteExiste";
-    public static final String COMPTE_EXISTE_FR = "Le compte existe déjà, le compte ne s'est pas créé";
-    public static final String COMPTE_EXISTE_EN = "The account already exists, the account has not been created";
     public static final String PSEUDO_EXISTE_KEY = "pseudoExiste";
-    public static final String PSEUDO_EXISTE_FR = "Le pseudo existe déjà, le compte ne s'est pas créé";
-    public static final String PSEUDO_EXISTE_EN = "The pseudonym already exists, the account has not been created";
     public static final String COMPTE_ERROR_KEY = "compteError";
-    public static final String COMPTE_ERROR_FR = "Une erreur est survenue, le compte ne s'est pas créé";
-    public static final String COMPTE_ERROR_EN = "An error has occurred, the account has not been created";
-    public static final String PASSWORD_DIFFERENT_KEY = "passwordDifferent";
-    public static final String PASSWORD_DIFFERENT_FR = "Le mot de passe n'a pas etait mit a jour, l'ancien mot de passe est incorrect.";
-    public static final String PASSWORD_DIFFERENT_EN = "The password was not updated, the old password is incorrect.";
     public static final String MOT_DE_PASSE_OUBLIE_P1_KEY = "motDePasseOublie_P1";
     public static final String MOT_DE_PASSE_OUBLIE_P2_KEY = "motDePasseOublie_P2";
 
@@ -83,17 +26,17 @@ public class Constantes {
     public static final String API_COMPTE_PASSWORD_UPDATE_SUCCES_KEY = "api.compte.password.update.success";
 
     // Messages d'erreur (champs obligatoires)
-    public static final String API_COMPTE_EMAIL_OBLIGATOIRE_KEY = "api.compte.cossy.obligatoire";  // Déjà correct
-    public static final String API_COMPTE_OLD_PASSWORD_OBLIGATOIRE_KEY = "api.compte.oldPassword.obligatoire";  // Déjà correct
-    public static final String API_COMPTE_NEW_PASSWORD_TROP_COURT_KEY = "api.compte.newPassword.trop_court";  // Corrigé
+    public static final String API_COMPTE_EMAIL_OBLIGATOIRE_KEY = "api.compte.cossy.obligatoire";
+    public static final String API_COMPTE_OLD_PASSWORD_OBLIGATOIRE_KEY = "api.compte.oldPassword.obligatoire";
+    public static final String API_COMPTE_NEW_PASSWORD_TROP_COURT_KEY = "api.compte.newPassword.trop_court";
 
     // Messages d'erreur (logique métier)
-    public static final String API_COMPTE_PASSWORD_DIFFERENT_KEY = "api.compte.password.different";  // Corrigé
-    public static final String API_COMPTE_SUPPRESSION_ECHEC_KEY = "api.compte.suppression.echec";  // Déjà correct
-    public static final String API_COMPTE_ACTIVATION_SUCCES_KEY = "api.compte.activation.succes";  // Déjà correct
-    public static final String API_COMPTE_ACTIVATION_ECHEC_KEY = "api.compte.activation.echec";  // Déjà correct
-    public static final String API_SECRET_INVALID_KEY = "api.secret.invalid";  // Déjà utilisé dans ton code
-    public static final String API_ERROR_GENERIC_KEY = "api.error.generic";  // Déjà utilisé dans ton code
+    public static final String API_COMPTE_PASSWORD_DIFFERENT_KEY = "api.compte.password.different";
+    public static final String API_COMPTE_SUPPRESSION_ECHEC_KEY = "api.compte.suppression.echec";
+    public static final String API_COMPTE_ACTIVATION_SUCCES_KEY = "api.compte.activation.succes";
+    public static final String API_COMPTE_ACTIVATION_ECHEC_KEY = "api.compte.activation.echec";
+    public static final String API_SECRET_INVALID_KEY = "api.secret.invalid";
+    public static final String API_ERROR_GENERIC_KEY = "api.error.generic";
 
 
 }
