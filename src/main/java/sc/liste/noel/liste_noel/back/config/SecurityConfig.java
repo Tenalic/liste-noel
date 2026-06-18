a961aee0600abd1322555f0b4ba4dc2aaac9afb0
tenalic <stephane.cocquebert@gmail.com>
2026-06-18 13:32:46 +0200

    migration react : health

--- src/main/java/sc/liste/noel/liste_noel/back/config/SecurityConfig.java
+++ src/main/java/sc/liste/noel/liste_noel/back/config/SecurityConfig.java
@@ -52,8 +52,8 @@ public class SecurityConfig {
                                 "/api/compte/**",
                                 "/api/liste/*",
                                 "/error",
-                                "/connexion",
-                                "/actuator/health"
+                                "/actuator/health",
+                                "/health"
                         ).permitAll()
                         // Toutes les autres routes nécessitent d'être connecté
                         .anyRequest().authenticated()
